9072704d305d6584ee3c2a5f52231f4115cc3d47
ortega-kb <kabweortega@gmail.com>
2026-01-17 18:36:52 +0200

    (feat): First commit

---
+++ src/main/java/com/ortegakb/onos_mcp_server/OnosMcpServerApplication.java
@@ -0,0 +1,13 @@
+package com.ortegakb.onos_mcp_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnosMcpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnosMcpServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ortegakb/onos_mcp_server/OnosMcpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ortegakb.onos_mcp_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnosMcpServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
